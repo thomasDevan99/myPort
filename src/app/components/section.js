7ba5d1f9740ab555f9de7fa3f191c3725ac4a1cb
Devan Thomas <thomasdevan99@gmail.com>
2025-05-16 17:37:40 -0400

    Temp: Flip section on click and show new text

--- src/app/components/section.js
+++ src/app/components/section.js
@@ -1,19 +1,37 @@
-import { motion } from "framer-motion";
+import { motion, animate, press } from "framer-motion";
+import { useState } from 'react';
 
 export default function Section({ name }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  // Handle box press
+  press(`.box-${name}`, (element) => {
+    animate(element, { scale: isFlipped ? 1.5 : 0.9 }, { type: "spring", stiffness: 500 });
+    return () => {
+      animate(element, { scale: isFlipped ? 1.5 : 1 }, { type: "spring", stiffness: 500 });
+      setIsFlipped((prev) => !prev); // Toggle flip
+
+    };
+  });
   return (
     <motion.div
-      drag
-      dragConstraints={{ left: -100, top: -100, right: 300, bottom: 100 }} 
+      className={`box-${name} content-center items-center justify-center`}
+      animate={{
+        rotateY: isFlipped ? 180 : 0, // Flip box 
+        scale: isFlipped ? 1.5 : 1, // Enlarge when flipped
+      }}
+      transition={{ duration: 1, ease: "easeInOut" }}
       style={{
         width: "100px",
         height: "100px",
-        backgroundColor: "#ff0088",
+        borderColor: "#ffffff",
+        backgroundColor: "#000000",
+        borderWidth: "2px",
         borderRadius: "10px",
         cursor: "grab",
       }}
     >
-      
+      <p className={'text-center'}>{isFlipped ? "New Text" : name}</p> 
     </motion.div>
   );
 }
